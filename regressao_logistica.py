eb47e6377df237ce15678d2ee1f9b19119331a04
PP <ppaulo030601@gmail.com>
2024-11-26 11:21:18 -0300

    NOTHING MUCH

--- regressao_logistica.py
+++ regressao_logistica.py
@@ -11,7 +11,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
 
-
 #Normalize the data
 def normalize_data(data):
     scaler = StandardScaler()
